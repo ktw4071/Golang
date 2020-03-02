a0dcecf0d9da76b7f83d75dd04469ecbf2e43e2e
Taewoo Kim <taewkim@cisco.com>
2020-03-02 10:08:36 -0500

    int key & string value

--- map/main.go
+++ map/main.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	// var colors map[string]string
 
-	colors := make(map[string]string)
+	colors := make(map[int]string)
 	// colors := map[string]string{
 	// 	"red":   "#ff0000",
 	// 	"green": "#4bf745",
 	// }
-	colors["white"] = "#ffffff"
+	colors[10] = "#ffffff"
 	fmt.Println(colors)
 }
