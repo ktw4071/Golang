ba606417cde1e249a4facaec048af7d900a07a82
Taewoo Kim <taewkim@cisco.com>
2020-03-02 10:08:05 -0500

    string key & string value

---
+++ map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+	colors["white"] = "#ffffff"
+	fmt.Println(colors)
+}
