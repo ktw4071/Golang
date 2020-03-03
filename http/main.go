edb363851a4dcbe62733850d1d6b25f67c82c318
Taewoo Kim <taewkim@cisco.com>
2020-03-03 09:42:35 -0500

    Custom Writer

--- http/main.go
+++ http/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+}
 
-	io.Copy(os.Stdout, resp.Body)
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
