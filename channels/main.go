b435a7238ad0bab3ba8050f6e0569f49ec8b1db3
Taewoo Kim <taewkim@cisco.com>
2020-03-04 09:21:07 -0500

    function literals

--- channels/main.go
+++ channels/main.go
@@ -33,9 +33,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "It's up!"
+	c <- link
 }
